ffd25284226d0806930c5b52f4bf997fe6ab1afd
Clark Tolosa <clarktolosa@gmail.com>
2022-04-07 22:15:35 +0800

    Modified animations

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 function update() {
-  const easy = document.querySelector('#easy .card')
-  const medium = document.querySelector('#medium .card')
-  const hard = document.querySelector('#hard .card')
+  easy = document.querySelector('#easy .card')
+  medium = document.querySelector('#medium .card')
+  hard = document.querySelector('#hard .card')
+
+  medRect = document.querySelector('#medium').getBoundingClientRect()
 
   const { scrollTop } = document.documentElement
 
-  if (scrollTop <= 937) {
-    easy.classList.add('fade-in')
+  if (medRect.y <= medRect.height && medRect.y >= 0) {
+    easy.classList.add('animate')
+    hard.classList.remove('animate')
   }
-  if (scrollTop >= 470) {
-    medium.classList.add('fade-in')
+  if (medRect.y <= Math.floor(medRect.height - medRect.height * 0.25)) {
+    medium.classList.add('animate')
   }
-  if (scrollTop >= 1400) {
-    hard.classList.add('fade-in')
+  if (medRect.y <= Math.floor(0 - medRect.height * 0.25)) {
+    hard.classList.add('animate')
+    easy.classList.remove('animate')
+  }
+  if (medRect.y == 937 || medRect.y == -937) {
+    medium.classList.remove('animate')
   }
 }
 
